b681bfcbd986bd8a7e25b2b5f31699e16e05ad44
Niklas Wojtkowiak <github.xeimw@passfwd.com>
2024-06-03 02:00:41 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -22,17 +22,26 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
         // put into into struct
+        // remove first 2 chars from the front if starts with '> '
+        input = input.trim_start_matches("> ").to_string();
+
+        // problem is that if the input is /usr/bin/ls it says not a command
+        // so we need to check if the input is a path and if it is, we need to run it
+        // if it is not a path, we need to run the command
+
+        // split command and args at space
+        let mut parts = input.trim().splitn(2, ' ');
+        let command_name = parts.next().unwrap();
+        let command_args = parts.next().unwrap_or("").trim();
+
         let command = Command {
-            // name is the first word
-            name: input.trim().split_whitespace().next().unwrap().to_string(),
-            // args are the rest of the words
-            args: input
-                .trim()
+            name: command_name.to_string(),
+            args: command_args
                 .split_whitespace()
-                .skip(1)
                 .map(|s| s.to_string())
                 .collect(),
         };
+
         // if check command not Type::None Run command
         if Type::None != check_command(command.name.as_str()) {
             run_command(&command);
@@ -55,7 +64,26 @@ fn run_command(command: &Command) {
             ),
             Type::None => println!("{}: not found", command.args[0]),
         },
-        _ => {}
+        "pwd" => println!("{}", std::env::current_dir().unwrap().display()),
+        "cd" => {
+            if command.args.len() == 0 {
+                std::env::set_current_dir(std::env::var("HOME").unwrap()).unwrap();
+            } else {
+                std::env::set_current_dir(command.args[0].as_str()).unwrap();
+            }
+        }
+        _ => {
+            // check if it is a path to a file or executable
+            // if it is, run it
+            // if not, run the command
+
+            let output = std::process::Command::new(&command.name)
+                .args(&command.args)
+                .output()
+                .expect("failed to execute process");
+            io::stdout().write_all(&output.stdout).unwrap();
+            io::stderr().write_all(&output.stderr).unwrap();
+        }
     }
 }
 
@@ -64,6 +92,8 @@ fn check_builtin(input: &str) -> bool {
         "exit" => true,
         "echo" => true,
         "type" => true,
+        "pwd" => true,
+        "cd" => true,
         _ => false,
     }
 }
@@ -83,11 +113,17 @@ fn check_command(input: &str) -> Type {
 fn check_path(input: &str) -> Option<String> {
     let path = env::var("PATH").unwrap();
     let paths: Vec<&str> = path.split(":").collect();
+    // add current directory to the path
+
     for p in paths {
         let full_path = format!("{}/{}", p, input);
         if std::path::Path::new(&full_path).exists() {
             return Some(full_path);
         }
+    }
+
+    if std::path::Path::new(input).exists() {
+        return Some(input.to_string());
     }
     None
 }
